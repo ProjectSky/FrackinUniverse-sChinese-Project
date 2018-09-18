489a96b2133606f6503c046347944fc122a75ec7
微风的龙骑士 风游迩 <2944262241@qq.com>
2018-09-18 22:33:36 +0800

    提交信息：参见内服的markdown文件

---
+++ ADD_FILES(Can_Be_Deleted)/ADD_TEMPLATE.md
@@ -0,0 +1,306 @@
+（补充：微风的龙骑士）
+
+多种翻译可酌情选择其中一种,如铁,铁制
+
+## [1.装备](#1)
+
+* [1.1抗性](#1.1)
+
+* [1.2属性](#1.2)
+
+* [1.3盾牌](#1.3)
+
+* [1.4武器](#1.4)
+
+* [1.5套装](#1.5)
+
+## [2.人物](#2)
+
+* [2.1属性](#2.1)
+
+## [3.环境](#3)
+
+* [3.1异常](#3.1)
+
+## [4.其他](#4)
+
+* [4.1符号说明](#4.1)
+
+* [4.2其他说明](#4.2)
+
+### <h4 id="1.1">1.1 抗性</h4>
+
+* 毒系抗性
+Poison Resistance
+* 电系抗性
+Electric Resistance
+* 火系抗性
+Fire Resistance
+* 冰系抗性
+Ice Resistance
+* 物理系抗性
+Physical Resist
+* 暗影系抗性
+Shadow Resist
+* 射线系抗性
+Cosmic Resist
+* 辐射系抗性
+Radiation Resist
+* 击退抗性
+Knockback Resist
+
+### <h4 id="1.2">1.2 属性</h4>
+
+* 伤害
+Damage
+* 防御
+Protection
+* 暴击几率
+Crit,Crit Chance,Critical Chance
+* 暴击伤害
+Crit Damage
+* 眩晕几率
+Stun Chance
+
+### <h4 id="1.3">1.3 盾牌</h4>
+
+* 盾牌防护值
+Shield Health
+* 盾牌防护值恢复速度
+Shield Regen
+* 盾牌完美格挡反伤
+Shield Bash
+* 盾牌击退
+Shield Push
+* 盾牌精通
+Shield Mastery
+
+### <h4 id="1.4">1.4 武器</h4>
+* 刃,之刃
+Blade
+* 矛,长矛
+Spear
+* 弓
+Bows
+* 弩
+Crossbow
+* 锤
+Hammer
+* 斧
+Axes
+* 巨斧
+Greataxe
+* 拳套
+Fists
+* 长棍
+Quarterstaff
+* 步枪
+Rifle
+* 手枪
+Pistol
+* 法杖
+Wands
+* 魔杖
+Staves
+* 宽剑
+Cleaver
+* 长剑
+Longsword
+* 短剑
+Shortsword
+* 短矛
+Shortspear
+* 匕首
+Daggers
+* 镰刀
+Scythe
+* 环刃
+Chakram
+* 鞭子
+Whips
+* 棱镜
+Prismatic
+* 回旋镖
+Boomerangs
+* 磁力珠
+Magnorb
+* 霰弹枪
+Shotguns
+* 钉头锤
+Mace
+* 打击者
+Striker
+* 狙击步枪
+Sniper Rifle
+* 自动手枪
+Machine Pistol
+* 能量武器
+Energy weapons
+* 近战武器
+Melee
+* 剑类武器
+Bladed weapons
+* 生物武器
+Bio-Weapon
+* 月晶武器
+Lunari weapons
+* 寂灭武器
+Quietus weapons
+* 粘液武器
+Slime weapons
+* 先驱者武器
+Precursor weapons
+* 狩猎武器
+Hunting weapons
+* 等离子武器
+Plasma weapon
+* 突击步枪
+Assault Rifle
+* 火箭发射器
+Rocket
+* 榴弹发射器
+Grenade Launcher
+* 激光挖掘器
+Mining Beam
+* 赛德瑞瑟武器
+Xithricite weapons
+
+### <h4 id="1.5">1.5 套装</h4>
+
+* 套装：X+/-*%，免疫X
+* 例: 套装：短剑伤害+10%，暴击伤害+20%，免疫黏液、炎热、致命高温
+
+### <h4 id="2.1">2.1 属性</h4>
+
+* 饥饿
+Hunger
+* 跳跃力
+Jump
+* 奔跑速度
+Speed
+* 游泳速度
+Swim
+* 掉落伤害
+Fall Damage
+* 生命值上限
+Health
+* 能量值上限
+Energy
+* 恢复速度
+Regen
+* 生命恢复速度
+Health Regen
+* 能量恢复速度
+Energy Regen
+* 黑暗恢复速度
+Darkness Regen
+
+### <h4 id="3.1">3.1 异常</h4>
+
+* 泥
+Mud
+* 冰
+* 雪
+Snow
+* 酸
+Acid
+* 火
+Fire
+* 液氮
+Liquid Nitrogen
+* 黏土
+Clay
+* 燃烧
+Burning
+* 毒素
+Poison
+* 硫酸
+Sulphuric acid,Sulfuric acid
+* 恐惧
+Terrified
+* 以太
+Aether
+* 辐射
+Radiation
+* 寒冷
+Cold
+* 炎热
+Heat
+* 岩浆
+Lava
+* 黏液
+Slime
+* 雪泥
+Slush
+* 蛛丝
+Freezed（可能）
+* 冻结
+Freezing（不是Freeze和Freezed）
+* 蜂蜇
+
+* 氧气
+Oxygen
+* 窒息
+Breath
+* 焦油
+Black Tar
+* 气体
+Gas
+* 脓液
+Pus
+* 软泥
+Oozed
+* 失重
+Zero-G
+* 压强
+Pressure
+* 暗影
+Shadow
+* 黑暗
+Darkness
+* 有机液
+Organic Soup
+* 致命辐射
+Deadly radiation
+* 致命严寒（不是致命寒意）
+Deadly cold
+* 暗影气体
+Shadow Gas
+* 辐射烧伤
+Radiation Burn
+* 暗影污染
+Shadow Taint
+* 致命炎热（由致命高温 改来）
+Deadly Heat
+* 沙漠炎热
+Desert Heat
+* 丛林炎热
+Jungle Heat
+* 生物软泥
+Bio Ooze
+* 精神错乱，疯狂（由精神错乱 改来）
+Insanity,Madness
+* (原始毒素)原毒
+Proto-Poison
+
+### <h4 id="4.1">4.1 符号说明</h4>
+
+* 所有符号（句号、逗号、顿号、括号）请使用全角，而省略号则用半角三个点表示“...”
+
+### <h4 id="4.2">4.1 其他说明</h4>
+
+* 黏液、黏土发音为“nian”而非“zhan”
+* 汉化文本之间请勿留空，这样有可能会导致游戏显示文本错行
+* 文本“crit”应译为“暴击几率”，请参考尾随数值大小/护甲级别（tier1即为T1套装）更改其含义
+* 如使用机器翻译，为增加口语化以及可读性，可酌情根据个人理解添加额外用词
+* 如出现名词，请先搜索所有文本，根据是否已有该名词再进行中文化
+* 开启管理员命令 “/admin”
+* 刷新指定物品命令，X代表对应物品名，*代表数量 “/spawnitem X *”
+* 物品ID可查看FU文本，通常都在对应物品itemName项内，或默认为该物品文件名
+
+*****
+
+#### 其他待补充。注：为了增强可阅读性，后续请使用markdown编写文档。
+
+
+[本地编辑项目文件指南：](https://gitlab.imsky.cc:666/ProjectSky/FrackinUniverse-sChinese-Project/wikis/%E6%9C%AC%E5%9C%B0%E7%BC%96%E8%BE%91%E9%A1%B9%E7%9B%AE%E6%96%87%E4%BB%B6%E6%8C%87%E5%8D%97)
---
+++ ADD_FILES(Can_Be_Deleted)/select_json_files.py
@@ -0,0 +1,168 @@
+# 作者：微风的龙骑士
+# 时间：2018/8/14
+
+# 一个简陋的过滤脚本
+
+# 过滤掉不需要再进行翻译的Json文件，即只删除所有已经翻译完毕的Json文件。
+# 传入的参数名应为"Texts"
+# 放入translations文件夹下
+
+# 使用方式：依次输入指令1,2,3即可，输入指令0退出。
+
+# 进度：
+# + 初步实现
+# - 使用json模块进行过滤
+# - 提取需要的Chs-Eng键值对，存储到单独的Json文档中，翻译好后，可以逆向替换回去。
+# - 可以二次过滤还未翻译完的json文档
+
+import os
+
+
+class Handler:
+    # 默认的Texts路径
+    texts_path = ""
+    # 已翻译完的文档的路径列表
+    translated_json_docs = []
+    # 未翻译完的文档的路径列表
+    not_translated_json_docs = []
+
+    def __init__(self):
+        self.texts_path = os.getcwd()
+        if self.texts_path.endswith("ADD_FILES(Can_Be_Deleted)"):
+            self.texts_path = self.texts_path.rsplit(
+                "\\", 1)[0] + "\\translations\\texts"
+        elif self.texts_path.find("FrackinUniverse-Chinese-Project"):
+            self.texts_path = self.texts_path + "\\translations" + "\\texts"
+
+        print("当前选定的texts文件夹路径：" + self.texts_path)
+
+    def set_texts_path(self, path: str=None):
+        """设置工作路径（即Texts路径）"""
+    #     if not path:
+    #       self.texts_path = path
+        if path:
+            self.text_path = path
+
+    def _scan_dir_recursive(self, path):
+        """递归扫描指定的目录"""
+        # 扫描这个目录，得到所有子目录和子文件实体
+        with os.scandir(path) as it:
+            entry: os.DirEntry
+            # 对于每个实体
+            for entry in it:
+                # 如果这个实体是文件夹，则保留，并递归调用函数（DirEntry.is_dir()这个方法有问题）
+                # 如果没有包含点号则认为是文件夹，这时进行递归调用
+                if entry.is_dir and entry.name.find(".") == -1:
+                    self._scan_dir_recursive(entry.path)
+                # 如果这个实体是文件，则做进一步处理
+                else:
+                    # 如果是Json文件
+                    if entry.name.endswith((".json", ".JSON", ".Json")):
+                        self._handle_doc(entry)
+
+    def _handle_doc(self, entry: os.DirEntry):
+        """处理（分类）单个文档"""
+        # 打开这个文件，指定打开方式和编码
+        with open(entry.path, "r", encoding="UTF-8") as f:
+            # 通过f.readlines():list(str)得到每一行的文本，并进行遍历
+            lines = f.readlines()
+            flag = False
+            for i in range(len(lines) - 1):
+                # 如果某一行定位到Texts属性，且在下一行定位到Eng属性，而非Chs属性
+                if lines[i].find(r'"Texts":') != -1 \
+                        and lines[i + 1].find(r'"Eng":') != -1:
+                    # 标记这个文件为不需要删除的，跳出循环
+                    # 其他情况的Json文件都需要标记为要删除的
+                    flag = True
+                    break
+            # 将文档分组
+            if not flag:
+                self.translated_json_docs.append(entry)
+            else:
+                self.not_translated_json_docs.append(entry)
+
+    def delete_translated(self):
+        """删除已翻译完的文档"""
+        for doc in self.translated_json_docs:
+            os.remove(doc)
+        list.clear(self.translated_json_docs)
+        print("删除操作完成！")
+
+    def delete_not_translated(self):
+        """删除未翻译的文档"""
+        for doc in self.not_translated_json_docs:
+            os.remove(doc)
+        list.clear(self.not_translated_json_docs)
+        print("删除操作完成！")
+
+    def create_markup_files(self):
+        """为未翻译完的文件创建markup标记（空文件，.markup后缀）"""
+        for doc in self.not_translated_json_docs:
+            doc_markup_path = doc.path + ".markup"
+            with open(doc_markup_path, "w+"):
+                pass
+
+    def delete_markup_files(self):
+        for doc in self.not_translated_json_docs:
+            mu_path = doc.path + ".markup"
+            if os.path.exists(mu_path):
+                os.remove(mu_path)
+
+    def print_info(self):
+        """打印消息"""
+        print("已翻译的Json文件数目：", len(self.translated_json_docs))
+        print("未翻译的Json文件数目：", len(self.not_translated_json_docs))
+
+
+class Interface:
+    handler: Handler
+
+    def __init__(self):
+        self.handler = Handler()
+
+    def print_info(self):
+        """打印消息"""
+        print("""
+            1：设置路径（Texts，如果就在Texts下，可以忽略）
+            2：开始扫描（开始后面的操作前必须先进行扫描）
+            3：删除已经翻译完的文档（有危险）
+            4：删除未翻译完的文档（有危险）
+            5：为未翻译完的文档建立markup文档（空白文档）
+            6：删除所有的markup文档（空白文档）
+            0：退出
+        """)
+
+    def get_keyword(self, kw):
+        """根据指令进行不同的操作"""
+        if not kw:
+            return
+
+        if kw == "1":
+            path = input("请输入路径：")
+            self.handler.set_texts_path(path)
+        elif kw == "2":
+            self.handler._scan_dir_recursive(self.handler.texts_path)
+            self.handler.print_info()
+        elif kw == "3":
+            self.handler.delete_translated()
+            self.handler.print_info()
+        elif kw == "4":
+            self.handler.delete_not_translated()
+            self.handler.print_info()
+        elif kw == "5":
+            self.handler.create_markup_files()
+        elif kw == "6":
+            self.handler.delete_markup_files()
+        elif kw == "0":
+            exit(1)
+        else:
+            print("输入的指令不正确！")
+
+
+if __name__ == '__main__':
+    interface = Interface()
+    interface.print_info()
+    keyword: str
+    while True:
+        keyword = input("请输入指令：")
+        interface.get_keyword(keyword)
