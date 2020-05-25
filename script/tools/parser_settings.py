5130a3f7128768d245607d6c6d27cbc8873171f4
diskrubbish <234511.cool@163.com>
2020-05-25 09:38:44 +0800

    5-25 跟进翻译

--- script/tools/parser_settings.py
+++ script/tools/parser_settings.py
@@ -14,7 +14,7 @@ foi = {
                 "^beacon/.+$", "^safe/.+$", "^helpme/.+$", "^final/.+$", "^.+Text(/value)?$", "^gui.+/value$",
                 ".+Title$", "^paneLayout/.+/value$", "areYouSure/value$", "^blueprintUnlock$",
                 "^blueprintAlreadyKnown$", "^rotTimeDescriptions/.+/1$", "^[a-zA-Z]*[mM]essages/[a-zA-Z]+$",
-                ".+[mM]essage$", "^.*(hint|([a-zA-Z]*(Caption|[Tt]itle)))$", "^defaultPetNameLabel$",
+                ".+[mM]essage$", "^.*(hint|regex|([a-zA-Z]*(Caption|[Tt]itle)))$", "^defaultPetNameLabel$",
                 ".*descriptions/[0-9]+$", "^(un)?trackLabel$", "^modeTypeTextAndColor/[0-9]+/[0-9]+$"],
     "themes.config": ["^[0-9]+/1/[0-9]+/(0|(1/)?name)$"],
     "dungeonitems.config": ["^[0-9]+/1/[0-9]+/(0|(1/)?name)$"],
